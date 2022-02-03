bfee4c3232316983f56b6c7e714b5021419e26ca
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-02-03 14:35:50 +0100

    php taak 02

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,8 @@
+<?php
+
+ $persoon=array("Voornaam" => "willem","Achternaam" => "alexander","Leeftijd" => 53,"Opleiding" => "software developer");
+
+
+ echo "Gegevens Willem: Voornaam : " . $persoon ["Voornaam"] . " Achternaam: ".$persoon ["Achternaam"] . " leeftijd: " . $persoon ["Leeftijd"] . " Opleiding: " . $persoon ["Opleiding"];
+
+?>
