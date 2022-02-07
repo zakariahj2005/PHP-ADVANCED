b7c0e63f20bf631453b1fd90c561a28bb4707c15
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-02-07 14:47:53 +0100

    php loops 03

---
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -0,0 +1,9 @@
+<?php
+echo "<ul>";
+$dagen = array("maandag", "dinsadg", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+
+foreach($dagen as $dag){
+    echo "<li> $dag </li>";
+}
+echo "</ul>";
+?>
