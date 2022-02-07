60de8a7182cde6846781fadc58489ad19ecfffbf
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-02-07 11:33:46 +0100

    php loop 02

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,13 @@
+<?php 
+for($beginwaarde = 0; $beginwaarde <= 18; $beginwaarde++){
+
+    if($beginwaarde == 18){
+        echo "ik ben 18 jaar oud dus ik heb stemrecht!";
+    }
+    else{
+        echo "ik ben $beginwaarde jaar, dus ik mag nog niet stemmen" . "<br>";
+    }
+}
+
+
+?>
