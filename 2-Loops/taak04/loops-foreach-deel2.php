16098bcf3d3824fb305163c8ddf8b8128ac7eb02
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-02-08 18:51:58 +0100

    php loops 04

--- 2-Loops/taak04/loops-foreach-deel2.php
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -1,9 +1,10 @@
 <?php
 $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
-
-echo "<select>";
-  foreach ($klassen as $klas){
-      echo "<option value='$klas'>$klas</option>";
-  }
-echo "</select>";
+echo "<ul>";
+  echo "<select>";
+    foreach ($klassen as $klas){
+        echo "<li> <option value='$klas'>$klas</option> </li>";
+    }
+  echo "</select>";
+echo "</ul>";
 ?>
