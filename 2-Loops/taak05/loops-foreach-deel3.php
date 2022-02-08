ef8d19304862eb0b7f0b592a6d330d80b5092cee
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-02-08 21:09:19 +0100

    php loops 05

---
+++ 2-Loops/taak05/loops-foreach-deel3.php
@@ -0,0 +1,13 @@
+<?php
+$student  = array(
+    "voornaam"    => "Jan",
+    "achternaam"  => "Janssen",
+    "klas"        => "9A",
+    "Leeftijd"    => 17,
+    "Woonplaats"  => "Amstelveen"
+);
+
+foreach ($student as $key => $value){
+    echo " $value <br> ";
+}
+?>
