7702b762b1197731508a71c65b356b0134057def
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-02-08 14:11:53 +0100

    php loops 2.1

--- 2-Loops/taak01/loops-while.php
+++ 2-Loops/taak01/loops-while.php
@@ -1,12 +1,13 @@
 <?php
 $leeftijd = 0;
+echo "<ul>";
+    while($leeftijd <18){
+        echo "<li> ik ben $leeftijd jaar, dus ik mag nog niet stemmen. </li>";
+        $leeftijd++;
+    }
 
-while($leeftijd <18){
-    echo "ik ben $leeftijd jaar, dus ik mag nog niet stemmen." . "<br>";
-    $leeftijd++;
-}
-
-if($leeftijd==18){
-    echo "ik ben $leeftijd jaar oud dus ik heb stemrecht! ";
-}
+    if($leeftijd==18){
+        echo " <li> ik ben $leeftijd jaar oud dus ik heb stemrecht! </li>";
+    }
+echo "</ul>";
 ?>
