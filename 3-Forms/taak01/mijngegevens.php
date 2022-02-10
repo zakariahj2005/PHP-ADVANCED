eba433ac43bf96304681aade61ce5c038815780e
zakaria el hajjioui <zakaria.elhajjioui@student.rocva.nl>
2022-02-10 09:19:06 +0100

    php form 01

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,36 @@
+<?php
+if($_POST["voornaam"]){
+    echo $_POST['voornaam'];
+    echo "<br>";
+}
+
+if($_POST["achternaam"]){
+    echo $_POST['achternaam'];
+    echo "<br>";
+}
+
+if($_POST["klas"]){
+    echo $_POST['klas'];
+    echo "<br>";
+}
+
+if($_POST["leeftijd"]){
+    echo $_POST['leeftijd'];
+    echo "<br>";
+}
+
+if($_POST["adres"]){
+    echo $_POST['adres'];
+    echo "<br>";
+}
+
+if($_POST["plaatsnaam"]){
+    echo $_POST['plaatsnaam'];
+    echo "<br>";
+}
+
+if($_POST["favoriete_muziekband"]){
+    echo $_POST['favoriete_muziekband'];
+    echo "<br>";
+}
+?>
